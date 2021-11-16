7a8bd3e68adae90d834c3a877cc477e864361356
Shane Hoskin <codedashie@outlook.com>
2021-11-16 22:40:36 +1030

    og

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,11 +3,10 @@ const config = require('./src/config');
 module.exports = {
   siteMetadata: {
     title: 'Shane Hoskin',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+    description: 'Shane Hoskin is a unity programmer.',
+    siteUrl: 'https://shanehoskin.gatsbyjs.io/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '@',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
